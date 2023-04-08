11fd7913c165a8e904450967c9c1e407a49df58e
Gabriel Barros <gabrielbarross2311@gmail.com>
2023-04-07 22:46:48 -0300

    Migrate  histories criado

---
+++ src/database/knex/migrations/20230408014407_histories.js
@@ -0,0 +1,8 @@
+exports.up = (knex) => knex.schema.createTable("histories", (table) => {
+  table.increments('id')
+  table.integer('dish_id').references('id').inTable('dishes')
+  table.integer('user_id').references('id').inTable('users').onDelete("CASCADE")
+  table.timestamp('created_at').default(knex.fn.now())
+});
+
+exports.down = (knex) => knex.schema.dropTable("histories");
--- src/repositories/cartRespository.js
+++ src/repositories/cartRespository.js
@@ -3,15 +3,15 @@ const knex = require('../database/knex')
 class cartRepository {
   async create({dish_id, user_id}){
 
-    return await knex("cart").insert({dish_id, user_id})
+    return await knex("carts").insert({dish_id, user_id})
   }
 
   async get({user_id}){
-    return await knex("cart").where({user_id})
+    return await knex("carts").where({user_id})
   }
 
   async delete({cart_id}){
-    return await knex("cart").where({id:cart_id}).delete()
+    return await knex("carts").where({id:cart_id}).delete()
   }
 }
 
